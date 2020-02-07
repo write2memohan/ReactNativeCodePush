179949eca16847779864c3be59dd0ab623d0ea85
mradhakannan <mradhakannan@roomstogo.com>
2020-02-07 16:35:21 -0500

    Simplified the app

--- App.js
+++ App.js
@@ -1,140 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
+  AppRegistry,
+  Dimensions,
+  Image,
   StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
-  TouchableOpacity
+  TouchableOpacity,
+  View,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import CodePush from "react-native-code-push";
 
+class App extends Component {
 
-import codePush from "react-native-code-push";
+  render () {
+    return (
+  <View>
+      <Text>Hello world 1112sdsd!!</Text>
+      <TouchableOpacity onPress={()=> {CodePush.sync({
+      updateDialog: true,
+      installMode: CodePush.InstallMode.IMMEDIATE
+    });}}>
+        <Text>Check for updates</Text>
+      </TouchableOpacity>
+  </View>
 
+    );
+  }
+}
+//set check frequency options
+const codePushOptions = { checkFrequency: CodePush.CheckFrequency.ON_APP_START };
+const myApp = CodePush(codePushOptions)(App);
 
-const App: () => React$Node = () => {
-  return (
+AppRegistry.registerComponent('myApp', () => myApp);
 
-    
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-
-            <TouchableOpacity onPress={()=>{codePush.sync({
-                updateDialog: true,
-                installMode: codePush.InstallMode.IMMEDIATE
-              });}}>
-                      <Text>Check for updates</Text>
-                    </TouchableOpacity>
-
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-
-          
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-  // onButtonPress = () {
-  //   codePush.sync({
-  //     updateDialog: true,
-  //     installMode: codePush.InstallMode.IMMEDIATE
-  //   });
-  // }
-  
-};
-
-
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default myApp;
